695d48738161a492d37eb9fff1f56633f1b19a6c
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-07-31 17:26:24 +0600

    update base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import useAuthStore from '@/store/auth';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9090/api/v1',
+  baseURL: 'https://mtk-server-zeta.vercel.app/api/v1',
 });
 
 api.interceptors.request.use(

cb1bdf1c97cb331047581abab6dd117dd5399a1d
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-08-02 10:25:41 +0600

    add token management to useCart hook for conditional data fetching

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -2,11 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { getCart } from '@/api/product/getCart';
 import useCartStore from '@/store/cartStore';
 import { useEffect } from 'react';
+import useAuthStore from '@/store/auth';
 
 const useCart = () => {
+  const token = useAuthStore.getState().token;
   const { data, isLoading, isError, refetch } = useQuery({
     queryKey: ['cart'],
     queryFn: getCart,
+    enabled: !!token
   });
 
   const { setCart } = useCartStore(); 

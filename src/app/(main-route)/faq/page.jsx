f5fbbfe0fc90ca8094b2a81c5d6ef83aae6b115a
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-08-02 15:36:12 +0600

    faq page done

--- src/app/(main-route)/faq/page.jsx
+++ src/app/(main-route)/faq/page.jsx
@@ -8,14 +8,18 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion";
 import PageLayout from "@/components/layout/PageLayout";
-import { faqItems } from "@/data/data";
 import Link from "next/link";
+import { useGetFaq } from "@/hooks/useGetFaq";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const heroLinks = [
     { name: "Home", href: "/" },
     { name: "FAQs", isCurrent: true }
 ];
 const FAQ = () => {
+
+    const { FAQs, isLoading, isError } = useGetFaq();
+
     return (
         <div className="min-h-minus-header">
             <SimpleHero title="FAQs" links={heroLinks} />
@@ -28,20 +32,37 @@ const FAQ = () => {
                 {/* Accordion */}
                 <div className="max-w-4xl mx-auto">
                     <Accordion type="single" collapsible className="w-full">
-                        {faqItems.map((item) => (
+                        {isError && (
+                            <div className="bg-background rounded-sm border border-red-500 mb-4 p-4">
+                                <div className="flex justify-between items-center">
+                                    <span className="text-center text-red-500 flex-grow">Error feching FAQs</span>
+                                </div>
+                            </div>
+                        )}
+                        {isLoading && (
+                            [...Array(5)].map((_, index) => (
+                                <div key={index} className="bg-background rounded-sm border border-gray-200 mb-4 p-4">
+                                    <div className="flex justify-between items-center">
+                                        <Skeleton className="h-6 flex-grow" />
+                                        <Skeleton className="h-5 w-5 ml-4" />
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                        {FAQs?.map((FAQ) => (
                             <AccordionItem
-                                key={item.id}
-                                value={item.id}
-                                className="rounded-sm border border-gray-200 data-[state=open]:border-primary mb-4 py-4"
+                                key={FAQ?._id}
+                                value={FAQ?._id}
+                                className="bg-background rounded-sm border border-gray-200 data-[state=open]:border-primary mb-4 py-4"
                             >
                                 <AccordionTrigger
                                     className="px-6 py-4 text-lg font-medium text-title hover:no-underline flex justify-between items-center w-full rounded-sm transition-colors duration-200
                                     data-[state=open]:rounded-b-none"
                                 >
-                                    <span className="text-left flex-grow">{item.question}</span>
+                                    <span className="text-left flex-grow">{FAQ?.question}</span>
                                 </AccordionTrigger>
                                 <AccordionContent className="px-6 pb-6 pt-0 text-subtitle">
-                                    {item.answer}
+                                    {FAQ?.answer}
                                 </AccordionContent>
                             </AccordionItem>
                         ))}

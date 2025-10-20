5c43097453afc3c143336598e4ff5ce50c353d60
goni715 <halesgoni715149123@gmail.com>
2025-10-20 18:17:07 +0600

    update base_url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,8 +3,8 @@ import useAuthStore from '@/store/auth';
 import { toast } from 'sonner';
 
 const api = axios.create({
-  //baseURL: 'https://backend.triplemcompany.com/api/v1',
-  baseURL: 'http://localhost:9090/api/v1',
+  //baseURL: 'http://localhost:9090/api/v1',
+  baseURL: 'https://mtk-server-zeta.vercel.app/api/v1'
 });
 
 api.interceptors.request.use(
